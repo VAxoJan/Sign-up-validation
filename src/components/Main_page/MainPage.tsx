ec2ea3d776fe00d454f31cc92a0a387f156f3915
vaxo <vaxojaniashvili186@gmail.com>
2024-04-04 23:19:01 +0400

    Project-Finished

--- src/components/Main_page/MainPage.tsx
+++ src/components/Main_page/MainPage.tsx
@@ -1,85 +1,208 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-// import errorImg from '../Images/error.svg'
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import errorImage from "../Images/error.svg";
 
 const validationSchema = Yup.object({
-    firstName: Yup.string()
-        .required('Required')
-        .max(15, 'Must be 15 characters or less'),
-    lastName: Yup.string()
-        .required('Required')
-        .max(20, 'Must be 20 characters or less'),
-    email: Yup.string()
-        .required('Required')
-        .email('Invalid email address'),
-    password: Yup.string()
-        .required('Required')
-        .min(8, 'Password must be at least 8 characters')
+  firstName: Yup.string()
+    .required("Required")
+    .max(15, "Must be 15 characters or less"),
+  lastName: Yup.string()
+    .required("Required")
+    .max(20, "Must be 20 characters or less"),
+  email: Yup.string().required("Required").email("Invalid email address"),
+  password: Yup.string()
+    .required("Required")
+    .min(8, "Password must be at least 8 characters"),
 });
 
 const MainPage: React.FC = () => {
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: ''
-        },
-        validationSchema: validationSchema,
-        onSubmit: values => {
-            localStorage .setItem("user", JSON.stringify(values));
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      localStorage.setItem("user", JSON.stringify(values));
+    },
+  });
 
-        },
-    });
-
-    return (
-        <div className="flex justify-center items-center w-screen h-screen">
-            <div className="flex flex-col md:flex-row w-full max-w-[1200px] h-auto md:h-[600px] justify-between">
-                <div className="flex flex-col justify-center items-center w-full md:w-[500px] my-8 md:my-0 md:ml-[50px] font-mainFont">
-                    <h1 className="text-white font-bold text-[50px] md:text-[60px] leading-[50px] md:leading-[60px] text-center">Learn to code by watching others</h1>
-                    <p className="text-white font-medium mt-6 text-center">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
-                </div>
-                <div className="flex flex-col justify-center items-center md:items-start w-full md:w-[540px]">
-                    <div className="flex w-[540px] justify-center flex-col md:flex-row  bg-[#5E54A4] font-mainFont text-white gap-2 py-4">
-                        <h1 className="font-bold text-center md:text-left">Try it free for 7 days</h1>
-                        <p className="font-med text-center md:text-left">then $20/mo. thereafter</p>
-                    </div>
-                    <div className="w-full bg-white rounded-md my-[20px] px-[40px]">
-                        <form onSubmit={formik.handleSubmit} className="grid gap-[20px] py-[40px]">
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.firstName} className="border shadow-xl w-full h-[56px] outline-none px-[32px]" type="text" placeholder="First Name" name='firstName' />
-                            {formik.touched.firstName && formik.errors.firstName ? (
-                                <div>
-                                    <p className='flex justify-end text-[#FF7979] text-[11px]'>First Name cannot be empty</p>
-                                </div>
-                            ) : null}
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.lastName} className="border shadow-xl w-full h-[56px] outline-none px-[32px]" type="text" placeholder="Last Name" name='lastName' />
-                            {formik.touched.lastName && formik.errors.lastName ? (
-                                <div>
-                                    <p className='flex justify-end text-[#FF7979] text-[11px]'>Last Name cannot be empty</p>
-                                </div>
-                            ) : null}
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} className="border shadow-xl w-full h-[56px] outline-none px-[32px]" type="email" placeholder="Email Address" name='email' />
-                            {formik.touched.email && formik.errors.email ? (
-                                <div>
-                                    <p className='flex justify-end text-[#FF7979] text-[11px]'>Looks like this is not an email</p>
-                                </div>) : null}
-                            <input onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} className="border shadow-xl w-full h-[56px] outline-none px-[32px]" type="password" placeholder="Password" name='password' />
-                            {formik.touched.password && formik.errors.password ? (
-                                <div>
-                                    <p className='flex justify-end text-[#FF7979] text-[11px]'>Password cannot be empty</p>
-                                </div>) : null}
-                            <button className="border shadow-xl w-full h-[56px] rounded-md bg-[#38CC8B] text-white font-semibold font-mainFont" type="submit">CLAIM YOUR FREE TRIAL</button>
-                            <div className="flex text-[11px] justify-center gap-1">
-                                <p className="text-[#BAB7D4]">By clicking the button, you are agreeing to our </p>
-                                <p className="text-[#FF7979]"> Terms and Services</p>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="flex flex-col md:flex-row w-full max-w-[1200px] h-auto md:h-[600px] justify-between">
+        <div className="flex flex-col justify-center px-[20px] xl:px-0 items-center w-full md:w-[500px] my-8 md:my-0 md:ml-[50px] font-mainFont">
+          <h1 className="text-white font-bold text-[50px] md:text-[60px] leading-[50px] md:leading-[60px] text-center">
+            Learn to code by watching others
+          </h1>
+          <p className="text-white font-medium mt-6 text-center">
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how developers think
+            is invaluable.
+          </p>
         </div>
-    );
+        <div className="flex flex-col justify-center items-center md:items-start w-full md:w-[540px]">
+          <div className="flex w-full xl:w-[540px] justify-center flex-col md:flex-row  bg-[#5E54A4] font-mainFont text-white gap-2 py-4">
+            <h1 className="font-bold text-center md:text-left">
+              Try it free for 7 days
+            </h1>
+            <p className="font-med text-center md:text-left">
+              then $20/mo. thereafter
+            </p>
+          </div>
+          <div className="w-full bg-white rounded-md my-[20px] px-[40px]">
+            <form
+              onSubmit={formik.handleSubmit}
+              className="grid gap-[20px] py-[40px]"
+            >
+              <div className="relative">
+                <input
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.firstName}
+                  className={`border shadow-xl w-full h-[56px] outline-none px-[32px] ${
+                    formik.touched.firstName && formik.errors.firstName
+                      ? "border-red-300"
+                      : "border-white"
+                  }`}
+                  type="text"
+                  placeholder={
+                    formik.touched.firstName && formik.errors.firstName
+                      ? ""
+                      : "First Name"
+                  }
+                  name="firstName"
+                />
+                {formik.touched.firstName && formik.errors.firstName && (
+                  <img
+                    src={errorImage}
+                    alt="Error"
+                    className="absolute top-1/2 right-[12px] transform -translate-y-1/2 w-[20px] h-[20px]"
+                  />
+                )}
+              </div>
+              {formik.touched.firstName && formik.errors.firstName ? (
+                <p className="text-[#FF7979] text-[11px] text-right">
+                  First Name cannot be empty
+                </p>
+              ) : null}
+
+              <div className="relative">
+                <input
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.lastName}
+                  className={`border shadow-xl w-full h-[56px] outline-none px-[32px] ${
+                    formik.touched.lastName && formik.errors.lastName
+                      ? "border-red-300"
+                      : "border-white"
+                  }`}
+                  type="text"
+                  placeholder={
+                    formik.touched.lastName && formik.errors.lastName
+                      ? ""
+                      : "Last Name"
+                  }
+                  name="lastName"
+                />
+                {formik.touched.lastName && formik.errors.lastName && (
+                  <img
+                    src={errorImage}
+                    alt="Error"
+                    className="absolute top-1/2 right-[12px] transform -translate-y-1/2 w-[20px] h-[20px]"
+                  />
+                )}
+              </div>
+              {formik.touched.lastName && formik.errors.lastName ? (
+                <p className="text-[#FF7979] text-[11px] text-right">
+                  Last Name cannot be empty
+                </p>
+              ) : null}
+
+              <div className="relative">
+                <input
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  placeholder={
+                    formik.touched.email && formik.errors.email
+                      ? "email@example/com"
+                      : "Email"
+                  }
+                  className={`border shadow-xl w-full h-[56px] outline-none px-[32px] ${
+                    formik.touched.email && formik.errors.email
+                      ? "border-red-300"
+                      : "border-white"
+                  }`}
+                  type="email"
+                  name="email"
+                />
+                {formik.touched.email && formik.errors.email && (
+                  <img
+                    src={errorImage}
+                    alt="Error"
+                    className="absolute top-1/2 right-[12px] transform -translate-y-1/2 w-[20px] h-[20px]"
+                  />
+                )}
+              </div>
+              {formik.touched.email && formik.errors.email ? (
+                <p className="text-[#FF7979] text-[11px] text-right">
+                  Invalid email address
+                </p>
+              ) : null}
+
+              <div className="relative">
+                <input
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  className={`border shadow-xl w-full h-[56px] outline-none px-[32px] ${
+                    formik.touched.password && formik.errors.password
+                      ? "border-red-300"
+                      : "border-white"
+                  }`}
+                  type="password"
+                  placeholder={
+                    formik.touched.password && formik.errors.password
+                      ? ""
+                      : "Password"
+                  }
+                  name="password"
+                />
+                {formik.touched.password && formik.errors.password && (
+                  <img
+                    src={errorImage}
+                    alt="Error"
+                    className="absolute top-1/2 right-[12px] transform -translate-y-1/2 w-[20px] h-[20px]"
+                  />
+                )}
+              </div>
+              {formik.touched.password && formik.errors.password ? (
+                <p className="text-[#FF7979] text-[11px] text-right">
+                  Password cannot be empty
+                </p>
+              ) : null}
+
+              <button
+                className="border shadow-xl w-full h-[56px] rounded-md bg-[#38CC8B] text-white font-semibold font-mainFont"
+                type="submit"
+              >
+                CLAIM YOUR FREE TRIAL
+              </button>
+              <div className="flex text-[11px] justify-center gap-1">
+                <p className="text-[#BAB7D4]">
+                  By clicking the button, you are agreeing to our{" "}
+                </p>
+                <p className="text-[#FF7979]"> Terms and Services</p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
