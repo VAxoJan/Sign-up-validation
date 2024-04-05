0e31553324f13e586e565da54cb407234b53b78d
vaxo <vaxojaniashvili186@gmail.com>
2024-04-05 18:18:47 +0400

    fixed-mobile-responsive-bug

--- src/components/_atoms/LeftSide/LeftSide.tsx
+++ src/components/_atoms/LeftSide/LeftSide.tsx
@@ -1,15 +1,16 @@
 const LeftSide = () => {
   return (
     <div className="flex flex-col justify-center px-[20px] xl:px-0 items-center w-full md:w-[500px] my-8 md:my-0 md:ml-[50px] font-mainFont">
-    <h1 className="text-white font-bold xl:text-[50px] text-[30px] xl:mt-0 mt-[130px] md:text-[60px] leading-[50px] md:leading-[60px] text-center">
-      Learn to code by watching others
-    </h1>
-    <p className="text-white font-medium mt-6 text-center">
-      See how experienced developers solve problems in real-time. Watching
-      scripted tutorials is great, but understanding how developers think
-      is invaluable.
-    </p>
-  </div>  )
-}
+      <h1 className="text-white font-bold xl:text-[50px] text-[20px] leading-7 xl:mt-0 mt-[130px] md:text-[60px] xl:leading-[50px] md:leading-[60px] text-center">
+        Learn to code by watching others
+      </h1>
+      <p className="text-white font-medium mt-6 text-center">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+    </div>
+  );
+};
 
 export default LeftSide;
